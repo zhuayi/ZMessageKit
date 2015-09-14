a22ab31fe92a9a8139e6b414eed6f08760e134d9
zhuayi <2179942@qq.com>
2015-09-14 21:57:45 +0800

    first commit

---
+++ ZMessageKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZMessageKit
+//
+//  Created by zhuayi on 9/14/15.
+//  Copyright (c) 2015 renxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZMessageKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZMessageKit
+//
+//  Created by zhuayi on 9/14/15.
+//  Copyright (c) 2015 renxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
